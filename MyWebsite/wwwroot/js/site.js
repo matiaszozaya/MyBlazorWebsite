93706b41a3e881ea9bd76b09e4b9c7c3ba76e989
Matías Zozaya <68297766+matiaszozaya@users.noreply.github.com>
2024-11-10 18:48:34 -0300

    Add project files.

---
+++ MyWebsite/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿function setMainContentHeight() {
+    const header = document.getElementById("header");
+    const footer = document.getElementById("footer");
+    const mainContent = document.getElementById("main");
+
+    function adjustHeight() {
+        const headerHeight = header.offsetHeight;
+        const footerHeight = footer.offsetHeight;
+        mainContent.style.height = `calc(100vh - ${headerHeight + footerHeight}px)`;
+    }
+
+    adjustHeight();
+}
+
+function addResizeListener() {
+    window.addEventListener("resize", setMainContentHeight);
+}
+
+function removeResizeListener() {
+    window.removeEventListener("resize", setMainContentHeight);
+}
